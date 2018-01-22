e9311d80b16a2f6364efc2829aa8aac61f8b678e
mcaikovs <marian.caikovski@gmail.com>
2018-01-22 12:51:47 +0100

    added interpretation to import

--- src/main/webapp/my.js
+++ src/main/webapp/my.js
@@ -15,14 +15,16 @@ function startApp() {
             fetch_basic_profile: false, // By default, the fetch_basic_profile parameter of gapi.auth2.init() is set to true, which will automatically add 'email profile openid' as scope.
             scope: 'email',
             ux_mode: 'redirect', // The UX mode to use for the sign-in flow. By default, it will open the consent flow in a popup. Valid values are popup and redirect.
-            redirect_uri: 'http://localhost:8080/mavenproject14/' //If using ux_mode='redirect', this parameter allows you to override the default redirect_uri that will be used at the end of the consent flow. 
+            redirect_uri: 'http://localhost:8080/test/' //If using ux_mode='redirect', this parameter allows you to override the default redirect_uri that will be used at the end of the consent flow. 
                     //// The default redirect_uri is the current URL stripped of query parameters and hash fragment.
         }).then(function (googleAuth) {
             console.log('init');
             auth2 = googleAuth;
-            auth2.then(function () {
-                console.log(">loaded googleAuth");
-                // Use the then() method to get a Promise that is resolved when the gapi.auth2.GoogleAuth object finishes initializing.
+            // Use the then() method to get a Promise that is resolved when the gapi.auth2.GoogleAuth object finishes initializing.
+            auth2.then(function (auth2) {
+                console.log(">loaded googleAuth: "+auth2);
+                
+
                 checkIfUserLoggedIn(); // this happens after redirect
             }, function (error) {
                 console.log(error); // if GoogleAuth failed to initialize.
@@ -32,9 +34,16 @@ function startApp() {
 }
 $(function () {
     console.log("ready");
-    $('#signIn').click(onClickSignIn);
+    var $signInButton = $('#googleSignIn');
+    $signInButton.click(onClickSignIn);
+
+    function hideSignInButton() {
+        $signInButton.hide();
+    }
+
     startApp();
 });
+
 
 function checkIfUserLoggedIn() {
     console.log(">checkIfUserLoggedIn");
